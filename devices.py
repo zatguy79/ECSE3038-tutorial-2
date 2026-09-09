35d965aa65580c2ccf0a42c2c489cef14466a0cd
zachabrahams11@gmail.com <zatguy79>
2026-09-09 15:00:46 -0500

    task 1: list devices

--- devices.py
+++ devices.py
@@ -5,3 +5,16 @@ readings = [
     {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
     {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
 ]
+x = 0
+devices = ["name", "temp"]
+def list_devices(T):
+        for index, item in enumerate(readings):
+            for key, value in readings[index].items():
+                if key == T[0]:
+                    print(f"{value}", end=" ")
+                if key == T[1]:
+                    print(f"{value}")   
+
+list_devices(devices)
+
+
