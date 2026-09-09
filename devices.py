c7335390243a0c45989813185dbddfeec486a287
zachabrahams11@gmail.com <zatguy79>
2026-09-09 14:24:38 -0500

    task 0: device data

---
+++ devices.py
@@ -0,0 +1,7 @@
+readings = [
+    {"name": "front-door", "room": "hall",    "temp": 27.4, "online": True},
+    {"name": "hall-lamp",  "room": "hall",    "temp": 26.1, "online": True},
+    {"name": "attic",      "room": "attic",   "temp": 31.9, "online": True},
+    {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
+    {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
+]
