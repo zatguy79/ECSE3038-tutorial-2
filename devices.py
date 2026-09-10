a645cb239dc2b73ba97ac0fa36855cc3513f92fa
zachabrahams11@gmail.com <zatguy79>
2026-09-10 18:00:38 -0500

    task 4: status object

--- devices.py
+++ devices.py
@@ -8,6 +8,15 @@ readings = [
 
 x = 0
 devices = ["name", "room", "temp", "online"]
+dev = []
+
+for index, item in enumerate(readings):
+    for key, value in readings[index].items():
+        if key == devices[0]:
+            dev.append(value)
+print(dev)
+
+device = dev[3]
 
 def list_devices(T):
         for index, item in enumerate(readings):
@@ -37,11 +46,30 @@ def hottest(E):
                         T_temp_dict = readings[index]
         print(T_temp_dict)
 
+readings_loop = readings.copy()
+
+def to_status(W):
+        for index, item in enumerate(readings_loop):
+            for key, value in readings_loop[index].items():
+                if value == device:
+                   if readings[index]["online"] == False:
+                        dev_dict = dict(device = (device), status = "offline", celcius = (readings[index]["temp"]))
+                   else:
+                        dev_dict = dict(device = (device), status = "ok", celcius = (readings[index]["temp"]))                    
+                   print(dev_dict)
+                   break
+                  
+
 list_devices(devices)
 
 average_temp(devices)
 
 hottest(devices)
+
+to_status(device)
+
+
+
 
 
 
